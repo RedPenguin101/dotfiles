d23ddb1857aa734491f7ca0cf745fbf6f9c71266
Joe Lezala <allaboutthatmace1789@gmail.com>
2021-12-05 11:31:54 +0000

    add i3/deps/bash to backup

--- dotfiles_backup.sh
+++ dotfiles_backup.sh
@@ -1,3 +1,6 @@
 cp ~/.config/nvim/init.vim ~/dotfiles/nvim/
 cp ~/.config/nvim/plugins/* ~/dotfiles/nvim/plugins
 cp ~/.config/kitty/* ~/dotfiles/kitty/
+cp ~/.config/i3/* ~/dotfiles/i3/
+cp ~/.clojure/deps.edn ~/dotfiles/
+cp ~/.bashrc ~/dotfiles/
