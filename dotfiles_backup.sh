31a26cde090f979d16681fd1ffdb602d6ba65c1b
Joe Lezala <allaboutthatmace1789@gmail.com>
2021-12-05 12:36:22 +0000

    change structure

--- dotfiles_backup.sh
+++ dotfiles_backup.sh
@@ -1,7 +1,8 @@
-cp ~/.config/nvim/init.vim ~/dotfiles/nvim/
-cp ~/.config/nvim/plugins/* ~/dotfiles/nvim/plugins
-cp ~/.config/kitty/* ~/dotfiles/kitty/
-cp ~/.config/i3/* ~/dotfiles/i3/
+cp ~/.config/nvim/init.vim ~/dotfiles/config/nvim/
+cp ~/.config/nvim/plugins/* ~/dotfiles/config/nvim/plugins
+cp ~/.config/kitty/* ~/dotfiles/config/kitty/
+cp ~/.config/i3/* ~/dotfiles/config/i3/
+cp ~/.config/i3status/* ~/dotfiles/config/i3status/
 cp ~/.clojure/deps.edn ~/dotfiles/
 cp ~/.bashrc ~/dotfiles/
-cp ~/.config/rofi/* ~/dotfiles/rofi
+cp ~/.config/rofi/* ~/dotfiles/config/rofi
