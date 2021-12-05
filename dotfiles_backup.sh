9335575c0016425b578b25133e2bea2c12d1b0c3
Joe Lezala <allaboutthatmace1789@gmail.com>
2021-12-05 13:01:26 +0000

    zsh

--- dotfiles_backup.sh
+++ dotfiles_backup.sh
@@ -3,6 +3,8 @@ cp ~/.config/nvim/plugins/* ~/dotfiles/config/nvim/plugins
 cp ~/.config/kitty/* ~/dotfiles/config/kitty/
 cp ~/.config/i3/* ~/dotfiles/config/i3/
 cp ~/.config/i3status/* ~/dotfiles/config/i3status/
+cp ~/.config/rofi/* ~/dotfiles/config/rofi
+
 cp ~/.clojure/deps.edn ~/dotfiles/
 cp ~/.bashrc ~/dotfiles/
-cp ~/.config/rofi/* ~/dotfiles/config/rofi
+cp ~/.zshrc ~/dotfiles/
