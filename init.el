44574718675a1e4bb09927c84891a432365d22d4
Joe Lezala <allaboutthatmace1789@gmail.com>
2023-04-24 22:12:32 +0100

    some tidy, mostly face config

--- init.el
+++ init.el
@@ -1,3 +1,15 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Joe's Emacs init.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Things to do or try
+;;
+;; - dumb-jump:a package which jumps from symbol to definition without
+;;   being language specific
+;; - a completion frontend. The built in is pretty terrible. Ivy, maybe.
+;; - Org mode - maybe. Big commitment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;;;;;;;;;;;;;;
 ;; the basics
 ;;;;;;;;;;;;;;
@@ -14,6 +26,10 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+;; dired-x enables `dired-do-find-marked-files` with `F`, which opens
+;; all marked files in new windows
+(require 'dired-x)
+
 ;;;;;;;;;;;;;;;;
 ;; recent mode
 ;;;;;;;;;;;;;;;;
@@ -27,18 +43,27 @@
 ;; keybinds
 ;;;;;;;;;;;;;;;;;
 
-;; This is to remind me of the basic navigations
-;; C-h n replaces emacs news
+;; This is to remind me of the basic navigations C-h n replaces emacs
+;; news
 (global-set-key (kbd "C-h n")
 		(lambda ()
 		  (interactive)
 		  (message "    C    M    CM\nfb char word defn\nae line sent sexp")))
 
 (global-set-key (kbd "M-o") 'other-window)
-(global-set-key (kbd "C-x C-b") 'ibuffer) ;; replace list-buffer. Suggestion from Mastering Emacs. ibuffer is just better.
-(global-set-key (kbd "C-w") 'backward-kill-word) ;; yegge suggestion
 
-(global-set-key (kbd "C-M-y") 'yank) ;; retain tempo when killing sexps with C-M-K
+;; replaces the default list-buffer with the more usable
+;; ibuffer. Suggestion from Mastering emacs
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; A Yegge suggestion to rebind backward-kill-word. The default
+;; bindings of this are C/M-<backspace>/<del>. This replaces the
+;; occasionally useful kill-region, which I should proabably bind to
+;; something else, but haven't needed to yet.
+(global-set-key (kbd "C-w") 'backward-kill-word)
+
+;; This allows retention of tempo when kill-yanking sexps
+(global-set-key (kbd "C-M-y") 'yank)
 
 ;; potential things to keybind (from Yegge)
 ;;   beginning and end of buffer
@@ -52,6 +77,12 @@
 
 (setq markdown-fontify-code-blocks-natively t)
 (add-hook 'markdown-mode-hook 'visual-line-mode)
+(add-hook 'markdown-mode-hook 'visual-fill-column-mode)
+(add-hook 'markdown-mode-hook 'adaptive-wrap-prefix-mode)
+
+;;;;;;;;;;;;;;
+;; org mode - uh oh
+;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;
 ;; clojure
@@ -74,9 +105,10 @@
  '(custom-safe-themes
    '("f149d9986497e8877e0bd1981d1bef8c8a6d35be7d82cba193ad7e46f0989f6a" "db86c52e18460fe10e750759b9077333f9414ed456dc94473f9cf188b197bc74" "fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" default))
  '(display-battery-mode t)
- '(menu-bar-mode nil)
+ '(display-time-mode t)
+ '(line-number-mode nil)
  '(package-selected-packages
-   '(adaptive-wrap visual-fill-column material-theme markdown-mode dracula-theme cider))
+   '(nov diff-hl adaptive-wrap visual-fill-column material-theme markdown-mode dracula-theme cider))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
@@ -86,5 +118,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 139 :width normal))))
+ '(fixed-pitch ((t (:family "Fira Code"))))
  '(markdown-code-face ((t (:foreground "#ffb86c" :family "Fira Code"))))
+ '(markdown-inline-code-face ((t (:foreground "#ffb86c" :family "Fira Code" :height 0.8))))
+ '(org-block ((t (:inherit fixed-pitch :extend t :background "#EFEBE9" :foreground "#212121"))))
+ '(org-code ((t (:inherit fixed-pitch :background "#EFEBE9" :foreground "#212121" :height 1.2))))
+ '(org-level-1 ((t (:inherit font-lock-function-name-face :extend nil :background "--" :box nil :weight bold :height 1.3))))
+ '(org-level-2 ((t (:inherit font-lock-function-name-face :extend nil :background "--" :box nil :weight bold :height 1.2))))
+ '(org-level-3 ((t (:inherit font-lock-function-name-face :extend nil :weight bold :height 1.1))))
+ '(org-level-4 ((t (:inherit nil :extend nil :height 1.0))))
+ '(org-meta-line ((t (:inherit fixed-pitch :height 0.6))))
+ '(org-table ((t (:inherit fixed-pitch :background "#e0f7fa" :foreground "#1565c0"))))
+ '(org-verbatim ((t (:height 0.8 :family "Fira Code"))))
  '(variable-pitch ((t (:height 1.5 :family "ETBookOT")))))
