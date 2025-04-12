5f5f780e8367c705d875b5a034b56cdc488f1021
Joe Lezala <allaboutthatmace1789@gmail.com>
2025-04-12 14:44:02 +0100

    add some commands and hooks for minibuffer

--- modal.el
+++ modal.el
@@ -5,13 +5,50 @@
 (defvar my-modal-command-map (make-sparse-keymap))
 (defvar my-modal-insert-map  (make-sparse-keymap))
 
+;; Keybinds
+;;;;;;;;;;;;;;
+
 (define-key my-modal-insert-map  (kbd "<escape>") #'command-mode-init)
 (define-key my-modal-command-map (kbd "f") #'insert-mode-init)
 
 (define-key my-modal-insert-map  (kbd "C-c i") (lambda () (interactive) (message "In Insert Mode")))
 (define-key my-modal-command-map (kbd "C-c c") (lambda () (interactive) (message "In Command Mode")))
 
+;; Command mode commands
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar current-mode 'insert)
+
+(define-key my-modal-command-map (kbd "a") 'execute-extended-command)
+
+;; Moves
+
+(define-key my-modal-command-map (kbd "k") 'previous-line)
+(define-key my-modal-command-map (kbd "j") 'next-line)
+(define-key my-modal-command-map (kbd "h") 'backward-word)
+(define-key my-modal-command-map (kbd "l") 'forward-word)
+
+(define-key my-modal-command-map (kbd "u") 'move-beginning-of-line)
+(define-key my-modal-command-map (kbd "p") 'move-end-of-line)
+
+(define-key my-modal-command-map (kbd "'") 'recenter-top-bottom)
+
+;; Windows
+(define-key my-modal-command-map (kbd "o") 'other-window)
+(define-key my-modal-command-map (kbd "3") 'split-window-right)
+(define-key my-modal-command-map (kbd "2") 'split-window-below)
+(define-key my-modal-command-map (kbd "1") 'delete-other-windows)
+
+;; Edits
+(define-key my-modal-command-map (kbd "q") 'fill-paragraph)
+
+;; CUA
+(define-key my-modal-command-map (kbd "z") 'undo)
+(define-key my-modal-command-map (kbd "x") 'kill-region)
+(define-key my-modal-command-map (kbd "c") 'kill-ring-save)
+(define-key my-modal-command-map (kbd "v") 'yank)
+
+;; Activation
+;;;;;;;;;;;;;;;
 
 (defun update-key-map ()
   (set-keymap-parent my-modal-key-map
@@ -38,13 +75,20 @@
 (define-minor-mode my-modal-mode
   "My modal mode"
   :global t
-  :lighter "modal"
+  :lighter " Modal"
   :keymap my-modal-key-map
-  (insert-mode-init)
-  )
+  (if my-modal-mode
+      (progn
+        (add-hook 'minibuffer-setup-hook 'insert-mode-init)
+        (add-hook 'minibuffer-exit-hook 'command-mode-init)
+        (command-mode-init))
+    (progn
+      (remove-hook 'minibuffer-setup-hook 'insert-mode-init)
+      (remove-hook 'minibuffer-exit-hook 'command-mode-init)
+      (setq mode-line-front-space '(:eval (if (display-graphic-p) " " "-")))
+      (force-mode-line-update))))
+
+(provide 'my-modal-mode)
 
 (my-modal-mode 1)
-
-;;(command-mode-init)
-;;(current-active-maps)
-;;(insert-mode-init)
+;;(my-modal-mode 0)
